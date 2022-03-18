1c2aab7031405046232d48dfabee2d719ecdf5ce
Florian Mold <florian.mold@agnet.at>
2022-03-18 18:01:51 +0100

    feat(apps/reactive-architecture): add payment-module

--- apps/reactive-architecture/src/app/app.module.ts
+++ apps/reactive-architecture/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {PaymentModule} from "./payment/payment.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, PaymentModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/reactive-architecture/src/app/payment/payment.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PaymentComponent } from './payment.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [PaymentComponent],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatButtonModule,
+  ],
+  exports: [PaymentComponent],
+})
+export class PaymentModule {}
